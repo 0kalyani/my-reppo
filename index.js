09d5efa3198e59ec6966583d602bc40542614277
bcakalyani1999 <bcakalyani1999@gmail.com>
2023-04-13 14:30:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunc(){
+    var my=document.getElementById("a1");
+    const myUrl="https://www.google.com/imgres?imgurl=https%3A%2F%2Fhips.hearstapps.com%2Fhmg-prod%2Fimages%2Fgolden-retriever-royalty-free-image-506756303-1560962726.jpg%3Fcrop%3D1.00xw%3A0.756xh%3B0%2C0.0756xh%26resize%3D1200%3A*&tbnid=9Zv98XrDcfYCXM&vet=12ahUKEwjMlf6uuqb-AhUA1HMBHQ8NAxcQMygCegUIARDkAQ..i&imgrefurl=https%3A%2F%2Fwww.goodhousekeeping.com%2Flife%2Fpets%2Fadvice%2Fg1921%2Flarge-dog-breeds%2F&docid=FofFudZ0yWjNIM&w=1200&h=605&itg=1&q=dog&ved=2ahUKEwjMlf6uuqb-AhUA1HMBHQ8NAxcQMygCegUIARDkAQ"
+    my.innerHTML=`<h1>Hello World</h1> <div> <img src="download(1).jpg" height='100'/></div>`;
+    console.log(my);
+}
